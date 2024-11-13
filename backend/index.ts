eb61664cdbfe6bd1af35d822a7934c99e45d0431
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-11-13 10:40:45 +0100

    fixRouter

--- backend/index.ts
+++ backend/index.ts
@@ -7,21 +7,12 @@ import UserData from "./userType";
 import {connectDB} from "./connect_db";
 dotenv.config();
 import {getAllUsers} from "./getUser";
+import router from "./routes/user";
 
 connectDB(process.env.MONGODB_URL!).then();
 const app = express();
-const router = express.Router();
 app.use(express.json());
 app.use(router);
-router.get('/api/users', async (req: Request, res: Response) => {
-    try {
-        const users = await getAllUsers(); // Utilisation de votre fonction pour récupérer les utilisateurs
-        res.status(200).json(users); // Envoie les utilisateurs dans la réponse HTTP sous forme de JSON
-    } catch (error : any) {
-        res.status(500).json({ message: error.message }); // Gère les erreurs
-    }
-});
-app.post('/api/users', createUser);
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.error(err.stack);
     res.status(500).send({ message: 'Something went wrong!' });
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,10 +1,26 @@
-import { Router } from 'express';
-import {createUser} from "../createUser.ts";
-import {getAllUsers} from "../getUser.ts";
+import {Request, Response, Router} from 'express';
+import {createUser} from "../createUser";
+import {getAllUsers} from "../getUser";
 
 
 const router = Router();
 
+router.get('/api/users', async (req: Request, res: Response) => {
+    try {
+        const users = await getAllUsers(); // Utilisation de votre fonction pour récupérer les utilisateurs
+        res.status(200).json(users); // Envoie les utilisateurs dans la réponse HTTP sous forme de JSON
+    } catch (error : any) {
+        res.status(500).json({ message: error.message }); // Gère les erreurs
+    }
+});
 
+router.post('/api/users', async (req: Request, res: Response) => {
+    try {
+        const user = await createUser(req.body);
+        res.status(201).json(user);
+    } catch (error : any) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 export default router;
