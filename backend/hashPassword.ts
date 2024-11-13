8efe1c4bf41911143d15cd4f859a22fe32baeb02
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-11-13 15:30:31 +0100

    hashSaltPassword

---
+++ backend/hashPassword.ts
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt';
+
+async function hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+}
+
+export default hashPassword;
--- backend/schema/userSchema.ts
+++ backend/schema/userSchema.ts
@@ -1,3 +1,4 @@
+import hashPassword from "../hashPassword";
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
@@ -16,9 +17,33 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        match: [/\S+@\S+.\S+/, 'is invalid']
     }
 }, { timestamps: true });
 
+// Hash password before saving (on creation or modification)
+userSchema.pre('save', async function (next) {
+    const user = this as any;
+    if (!user.isModified('password')) return next();
 
-export default userSchema;
+    try {
+        user.password = await hashPassword(user.password);
+        next();
+    } catch (error:any) {
+        next(error);
+    }
+});
+
+// Hash password on update if password is being modified
+userSchema.pre('findOneAndUpdate', async function (next) {
+    const update = this.getUpdate() as any;
+    if (update.password) {
+        try {
+            update.password = await hashPassword(update.password);
+        } catch (error:any) {
+            return next(error);
+        }
+    }
+    next();
+});
+
+export default mongoose.model('User', userSchema);
