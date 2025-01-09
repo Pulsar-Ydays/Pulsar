02ef70982aa4605b9c541f98d81632e1be3cd703
AdRI3n30 <144777695+AdRI3n30@users.noreply.github.com>
2025-01-09 12:30:34 +0100

    Update-Wallet-Swap-SideBar

--- frontend/src/components/ui/wallettable.tsx
+++ frontend/src/components/ui/wallettable.tsx
@@ -18,34 +18,39 @@ type AssetTableProps = {
 const AssetTable: React.FC<AssetTableProps> = ({ assets }) => {
     return (
         <section className="bg-gray-800 rounded-lg p-6">
-        <div className="max-h-96 overflow-y-auto">
+        <div className="h-auto max-h-96 overflow-y-auto lg:max-h-[500px]"> 
           <div className="overflow-x-auto">
             <table className="w-full text-left text-sm sm:text-base">
               <thead>
                 <tr className="border-b border-gray-700 text-gray-400">
                   <th className="py-4 px-2">Assets</th>
-                  <th className="px-2">Price</th>
-                  <th className="px-2">Total Balance</th>
-                  <th className="px-2">24h Market</th>
+                  <th className="px-2  sm:table-cell">Price</th>
+                  <th className="px-2 hidden sm:table-cell">Total Balance</th>
+                  <th className="px-2  lg:table-cell">24h Market</th>
                 </tr>
               </thead>
               <tbody>
                 {assets.map((asset, idx) => (
-                  <WalletData
-                    key={idx}
-                    icon={asset.icon}
-                    name={asset.name}
-                    price={asset.price}
-                    balance={asset.balance}
-                    market={asset.market}
-                    color={asset.color}
-                  />
+                  <tr key={idx} className="border-b border-gray-700">
+                    <td className="py-4 px-2">
+                      <div className="flex items-center">
+                        <img src={asset.icon} alt={asset.name} className="w-6 h-6 mr-2" />
+                        {asset.name}
+                      </div>
+                    </td>
+                    <td className="px-2  sm:table-cell">{asset.price}</td>
+                    <td className="px-2 hidden sm:table-cell">{asset.balance}</td>
+                    <td className={`px-2 lg:table-cell text-${asset.color}-500`}>
+                        {asset.market} 
+                    </td>
+                  </tr>
                 ))}
               </tbody>
             </table>
           </div>
         </div>
       </section>
+      
       
 
       );
