3f8d9ee3661d2aba0c9ea2eef22bbfc881976385
Keil-ENZO <enzo.keil06@icloud.com>
2025-03-12 16:26:04 +0100

    feat: affichage des cryptos avec graphe

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 import { connectDB } from "./connect_db";
-import express from 'express';
+import express from "express";
 import routerUser from "./routes/user";
 import routerTransac from "./routes/transac";
 import routerWallet from "./routes/wallet";
 import routerMarket from "./routes/market";
-import { setupSwagger } from './swagger';
+import { setupSwagger } from "./swagger";
 
 dotenv.config();
 
@@ -21,11 +21,18 @@ app.use(routerMarket);
 // Configuration de Swagger
 setupSwagger(app);
 
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
     console.error(err.stack);
-    res.status(500).send({ message: 'Something went wrong!' });
-});
+    res.status(500).send({ message: "Something went wrong!" });
+  }
+);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+  console.log(`Server started on port ${process.env.PORT}`);
 });
--- backend/routes/market.ts
+++ backend/routes/market.ts
@@ -1,16 +1,15 @@
-import { Router, Request, Response } from "express";
-import { verifyToken } from "../middleware/authMiddleware";
+import { Request, Response, Router } from "express";
 import { getCryptoData } from "../services/coinMarketCapService";
 
 const router = Router();
 
-router.get('/api/crypto', verifyToken, async (req: Request, res: Response) => {
-    try {
-        const data = await getCryptoData();
-        res.status(200).json(data);
-    } catch (error: any) {
-        res.status(500).json({ message: error.message });
-    }
+router.get("/api/crypto", async (req: Request, res: Response) => {
+  try {
+    const data = await getCryptoData();
+    res.status(200).json(data);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
